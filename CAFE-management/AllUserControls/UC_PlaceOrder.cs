55ff80571bef8943d76b118437479d15f538d097
SanjeedaMim <72091175+SanjeedaMim@users.noreply.github.com>
2022-04-10 16:57:58 +0600

    Place Order(category)

--- CAFE-management/AllUserControls/UC_PlaceOrder.cs
+++ CAFE-management/AllUserControls/UC_PlaceOrder.cs
@@ -12,8 +12,8 @@ namespace CAFE_management.AllUserControls
 {
     public partial class UC_PlaceOrder : UserControl
     {
+
         function fn = new function();
-        String query;
         public UC_PlaceOrder()
         {
             InitializeComponent();
@@ -31,14 +31,14 @@ namespace CAFE_management.AllUserControls
 
         private void ComboCategory_SelectedIndexChanged(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
             String category = ComboCategory.Text;
-            query = "select from the items where category ='" + category + "'";
+            String query = "select name from items where category ='" + category + "'";
             DataSet ds = fn.getdata(query);
 
             for (int i = 0; i<ds.Tables[0].Rows.Count; i++)
             {
                 listBox1.Items.Add(ds.Tables[0].Rows[i][0].ToString());
-
             }
         }
     }
